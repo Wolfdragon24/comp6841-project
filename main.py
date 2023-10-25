26ff210c9162a5a1fd3b1ff8f6be24d35fccbc13
Wolfdragon24 <dragonbomber24@gmail.com>
2023-10-25 12:52:05 +1100

    Fixed url properly

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ aus = pytz.timezone('Australia/Sydney')
 challenges = {
     "osint": {
         "title": "Where was I taken?",
-        "desc": "I spy with my little eye... a world far down below. Name from whence the lens nigh, then the flag you seek you know.\n\nThe flag is in all caps separated by underscores, e.g. DOM_CTF{CENTRAL_STATION}. <a href='https://ctf.wolfdragon.me:5000/resources/osint.jpg'>Resources</a>",
+        "desc": "I spy with my little eye... a world far down below. Name from whence the lens nigh, then the flag you seek you know.\n\nThe flag is in all caps separated by underscores, e.g. DOM_CTF{CENTRAL_STATION}. <a href='http://ctf.wolfdragon.me:5000/resources/osint.jpg'>Resources</a>",
         "answer": "MERITON_SUITE_WORLD_TOWER"
     },
     "bufferformat": {
         "title": "One won't bring you far enough...",
-        "desc": "You've seen buffer overflows vulnerabilities, you've seen format strings vulnerabilities, but have you needed both?\n\nLocated at <code>nc ctf.wolfdragon.me 62401</code> is the answer you seek, exploring the binary locally first from <a href='https://ctf.wolfdragon.me:5000/resources/bufferformat'>Resources</a> is highly recommended!",
+        "desc": "You've seen buffer overflows vulnerabilities, you've seen format strings vulnerabilities, but have you needed both?\n\nLocated at <code>nc ctf.wolfdragon.me 62401</code> is the answer you seek, exploring the binary locally first from <a href='http://ctf.wolfdragon.me:5000/resources/bufferformat'>Resources</a> is highly recommended!",
         "answer": "TRAILBLAZED_THE_DOUBLE"
     },
     "stego": {
         "title": "Great views up here!",
-        "desc": "Up in the air again in yet another challenge. This time, keep your eyes out for clues and you might just spot the keys you're looking for. Your key will be enclosed in two keys, though what you have now might not be what you must use now...\n\n<a href='https://ctf.wolfdragon.me:5000/resources/stego.png'>Resources</a>",
+        "desc": "Up in the air again in yet another challenge. This time, keep your eyes out for clues and you might just spot the keys you're looking for. Your key will be enclosed in two keys, though what you have now might not be what you must use now...\n\n<a href='http://ctf.wolfdragon.me:5000/resources/stego.png'>Resources</a>",
         "answer": "HIDDEN_IN_PLANE_SIGHT"
     }
 }
